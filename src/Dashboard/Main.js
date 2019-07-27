cdd2e4ffd28e8f367ac50c0c94bfde3862f34dd1
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-07-27 23:05:23 +1000

    Modal at the centre

--- src/Dashboard/Main.js
+++ src/Dashboard/Main.js
@@ -7,6 +7,7 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Modal from '@material-ui/core/Modal';
 
 import Chart from "./Chart";
 import Deposits from "./Deposits";
@@ -101,41 +102,88 @@ const useStyles = makeStyles(theme => ({
     position: "fixed",
     margin: theme.spacing(1),
   },
+  paperM: {
+    position: 'absolute',
+    width: 300,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 4),
+    outline: 'none',
+  },
 }));
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    margin: 'auto',
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 export default function Dashboard() {
   const classes = useStyles();
-
+  const [modalStyle] = React.useState(getModalStyle);
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
-    <Grid container spacing={3}>
-      {/* Chart */}
-      <Grid item xs={12} md={8} lg={9}>
-        <Paper className={fixedHeightPaper}>
-          <Chart />
-        </Paper>
-      </Grid>
-      {/* Recent Deposits */}
-      <Grid item xs={12} md={4} lg={3}>
-        <Paper className={fixedHeightPaper}>
-          <Deposits />
-        </Paper>
-      </Grid>
-      {/* Recent Orders */}
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>
-          <Orders />
-        </Paper>
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper className={fixedHeightPaper}>
+            <Chart />
+          </Paper>
+        </Grid>
+        {/* Recent Deposits */}
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper className={fixedHeightPaper}>
+            <Deposits />
+          </Paper>
+        </Grid>
+        {/* Recent Orders */}
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Orders />
+          </Paper>
 
+        </Grid>
       </Grid>
-    </Grid>
-    <div>
-    <Fab color="primary" aria-label="add" className={classes.fab} onClick= {() => {alert("HELLO")}}>
-        <AddIcon />
-    </Fab>
-    </div>
+      <div>
+        <Fab color="primary" aria-label="add" className={classes.fab} onClick={handleOpen}>
+          <AddIcon />
+        </Fab>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={handleClose}
+          style={{ alignItems: 'center', justifyContent: 'center' }}
+        >
+          <div style={modalStyle} className={classes.paperM}>
+            <h2 id="modal-title">One Bottle Recycled</h2>
+            <p id="simple-modal-description">
+              Keep them coming
+          </p>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 }

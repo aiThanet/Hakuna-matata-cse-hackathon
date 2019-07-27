7efca0e91ec203c2c18e289e29d20ab6e972e387
Thanet Sirichanyaphong <thanetsirichanyaphong@Thanets-MacBook-Pro.local>
2019-07-27 22:18:07 +1000

    update

--- src/Achievement/Achievement.js
+++ src/Achievement/Achievement.js
@@ -1,9 +1,32 @@
 import React from "react";
 
+import { SystemContext } from "../SystemContext.js";
+
 class Achievement extends React.Component {
+  static contextType = SystemContext;
+
+  test = () => {
+    this.context.current_user += 1;
+    console.log(this.context);
+  };
+
   render() {
-    return <div>Achievement Page</div>;
+    console.log(this.context);
+    return (
+      <div>
+        55 {this.context.current_user}
+        <button onClick={this.test}>click</button>
+      </div>
+    );
   }
 }
+
+// const Achievement = () => (
+//   <SystemContext.Consumer>
+//     <div>Achievement Page </div>
+//   </SystemContext.Consumer>
+// );
+
+// Achievement.comtextType = SystemContext;
 
 export default Achievement;
--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import { Switch, Route } from "react-router-dom";
 import Signin from "./Signin/Signin.js";
 import Signup from "./Signup/Signup.js";
 import Dashboard from "./Dashboard/Dashboard.js";
+import { SystemContext, system_context } from "./SystemContext";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/signin" component={Signin} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/dashboard" component={Dashboard} />
-    </Switch>
+    <SystemContext.Provider value={system_context}>
+      <Switch>
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </SystemContext.Provider>
   );
 }
 

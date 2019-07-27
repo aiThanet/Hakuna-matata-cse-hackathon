b6441d312964b9f7c75683b2b77fd760f98d2d78
Thanet Sirichanyaphong <thanetsirichanyaphong@Thanets-MacBook-Pro.local>
2019-07-27 17:30:02 +1000

    add simple pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+
+import Signin from "./Signin.js";
+import Signup from "./Signup.js";
+import Dashboard from "./Dashboard/Dashboard.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Switch>
   );
 }
 
